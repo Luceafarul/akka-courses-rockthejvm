4dccf7eb18f895c847229e94f4013cc083c937e8
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2021-07-16 00:01:00 +0300

    Add solution for Exercise with child actors.

--- 01-akka-essentials/src/main/scala/part02_actors/Exercise03.scala
+++ 01-akka-essentials/src/main/scala/part02_actors/Exercise03.scala
@@ -1,6 +1,8 @@
 package part02_actors
 
-import akka.actor.Actor
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+
+import scala.collection.immutable.Queue
 
 object Exercise03 extends App {
   // Distributed Word Counter
@@ -17,20 +19,69 @@ object Exercise03 extends App {
    *           <- WordCounterMaster <-
    *
    * Use round robin logic:
-   * 1, 2, 3, 4, 5 tasks and 7 workers
+   * 1, 2, 3, 4, 5 workers and 7 tasks
    * 1, 2, 3, 4, 5, 1, 2
    */
 
   class WordCounterMaster extends Actor {
-    override def receive: Receive = ???
+    import WordCounterMaster._
+
+    override def receive: Receive = {
+      case Initialize(n) =>
+        val workers =
+          for (i <- 1 to n)
+            yield context.actorOf(Props[WordCounterWorker], s"worker-$i")
+        context.become(workersReady(workers.toList, List.empty))
+    }
+
+    def workersReady(workers: List[ActorRef], tasks: List[String]): Receive = {
+      case message: String =>
+        if (workers.isEmpty) {
+          println("Waiting for available worker...")
+          context.become(workersReady(workers, tasks :+ message))
+        } else {
+          workers.head ! WordCountTask(message)
+          context.become(workersReady(workers.tail, tasks))
+        }
+      case WordCountReply(workerRef, wordCount) =>
+        println(s"Word count in ... $wordCount")
+        if (tasks.nonEmpty && workers.nonEmpty) {
+          workerRef ! WordCountTask(tasks.head)
+          context.become(workersReady(workers, tasks.tail))
+        } else {
+          context.become(workersReady(workers :+ workerRef, tasks))
+        }
+    }
   }
   object WordCounterMaster {
     final case class Initialize(nChildren: Int)
-    final case class WordCountTask(/*TODO*/ text: String)
-    final case class WordCountReply(/*TODO*/ count: Int)
+    final case class WordCountTask(text: String)
+    final case class WordCountReply(workerRef: ActorRef, count: Int)
   }
 
   class WordCounterWorker extends Actor {
-    override def receive: Receive = ???
+    import WordCounterMaster._
+
+    override def receive: Receive = {
+      case WordCountTask(text) =>
+        if (text.isEmpty) sender() ! WordCountReply(self, 0)
+        else {
+          println(s"${self.path.name}: counting words...")
+          val wordCount = text.split(" ").length
+          sender() ! WordCountReply(self, wordCount)
+        }
+    }
   }
+
+  val system = ActorSystem("distributed-word-counter")
+
+  import WordCounterMaster._
+  val wordCounterMaster =
+    system.actorOf(Props[WordCounterMaster], "word-counter-master")
+
+  wordCounterMaster ! Initialize(2)
+  wordCounterMaster ! "Hello darkness my old friend"
+  wordCounterMaster ! "Hello how are you?"
+  wordCounterMaster ! "I learn Akka! And I listen music when learn."
+  wordCounterMaster ! "I learn Scala!"
 }
